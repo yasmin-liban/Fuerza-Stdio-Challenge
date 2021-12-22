80b344f4fb04c104e53d8e4abcf42b7110243d94
Yasmin <flor.yasmin90@gmail.com>
2021-12-22 02:14:06 -0300

    hero component on the app

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,12 +5,14 @@ import './images';
 import App from './app';
 import Header from './components/header';
 import SubMenu from './components/submenu';
+import Hero from './components/hero';
 
 // Your code goes here ...
 document.addEventListener('DOMContentLoaded', () => {
   const app = new App([
     new Header(),
     new SubMenu(),
+    new Hero(),
   ]);
 
   app.bootstrap();
